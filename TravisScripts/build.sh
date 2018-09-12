472d11d79c3b6e5e81ef2c60aea7f93c535159e1
Jano van der Dussen <duss0001@hz.nl>
2018-09-12 14:08:29 +0200

    added possible projectPath fix

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -8,28 +8,28 @@ echo "Attempting to build $project for Windows"
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \ 
-  -projectPath $(pwd) \
+  -projectPath $(pwd)/$project \
   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
   -quit
 
 echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \  
+  -projectPath $(pwd)/$project \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
   -quit
 
 echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity 
-  -batchmode 
-  -nographics 
-  -silent-crashes 
-  -logFile $(pwd)/unity.log 
-  -projectPath $(pwd) 
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" 
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd)/$project \
+  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
   -quit
 
 echo 'Logs from build'
