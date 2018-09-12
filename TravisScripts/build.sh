bfa66e9dac3f7f56d000ffea8cdece60b9df6914
Jano van der Dussen <janovdd@gmail.com>
2018-09-12 15:01:38 +0200

    yadayada

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -1,31 +1,31 @@
 #! /bin/sh
 
-{UNITYCI_PROJECT_NAME}="Unity project"
+project="Unity project"
 
 ## Make the builds
 # Recall from install.sh that a separate module was needed for Windows build support
-echo "Attempting build of ${UNITYCI_PROJECT_NAME} for Windows"
+echo "Attempting build of $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 	-batchmode \
 	-nographics \
 	-silent-crashes \
 	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildWindowsPlayer "$(pwd)/Build/windows/${UNITYCI_PROJECT_NAME}.exe" \
+	-projectPath $(pwd) \
+	-buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
 	-quit
 
 rc1=$?
 echo "Build logs (Windows)"
 cat $(pwd)/unity.log
 
-echo "Attempting build of ${UNITYCI_PROJECT_NAME} for OSX"
+echo "Attempting build of $project for OSX"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
 	-batchmode \
 	-nographics \
 	-silent-crashes \
 	-logFile $(pwd)/unity.log \
-	-projectPath "$(pwd)/${UNITYCI_PROJECT_NAME}" \
-	-buildOSXUniversalPlayer "$(pwd)/Build/osx/${UNITYCI_PROJECT_NAME}.app" \
+	-projectPath $(pwd) \
+	-buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
 	-quit
 
 rc2=$?
