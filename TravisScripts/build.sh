a438f3ba28b36cc2978d25c5e3c6635ea0326880
Jano van der Dussen <janovdd@gmail.com>
2018-09-14 10:04:56 +0200

    Trying WebGL

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -4,29 +4,43 @@ project="Unity project"
 
 ## Make the builds
 # Recall from install.sh that a separate module was needed for Windows build support
-echo "Attempting build of $project for Windows"
-echo $(pwd)
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath $(pwd) \
-	-buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-	-quit
+
+# echo "Attempting build of $project for Windows"
+# echo $(pwd)
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	# -batchmode \
+	# -nographics \
+	# -silent-crashes \
+	# -logFile $(pwd)/unity.log \
+	# -projectPath $(pwd) \
+	# -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+	# -quit
 	
-echo "Build logs (Windows)"
-cat $(pwd)/unity.log
+# echo "Build logs (Windows)"
+# cat $(pwd)/unity.log
 
-echo "Attempting build of $project for OSX"
+# echo "Attempting build of $project for OSX"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+	# -batchmode \
+	# -nographics \
+	# -silent-crashes \
+	# -logFile $(pwd)/unity.log \
+	# -projectPath $(pwd) \
+	# -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+	# -quit
+
+# echo "Build logs (OSX)"
+# cat $(pwd)/unity.log
+
+echo "Attempting to build $project for WebGL"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-	-batchmode \
-	-nographics \
-	-silent-crashes \
-	-logFile $(pwd)/unity.log \
-	-projectPath $(pwd) \
-	-buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-	-quit
-
-echo "Build logs (OSX)"
-cat $(pwd)/unity.log
+ -batchmode \
+ -nographics \
+ -silent-crashes \
+ -logFile \
+ -projectPath $(pwd)/ \
+ -quit \
+ -executeMethod BuildScript.WebGL
+ 
+ echo "Build logs (WebGL)"
+ cat $(pwd)/unity.log
